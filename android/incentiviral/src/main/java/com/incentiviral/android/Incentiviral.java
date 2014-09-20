6bfbe10946329c6cc66656d0149b5970024625bd
Soham Mondal <soham@triveous.com>
2014-09-20 21:26:56 +0530

    User Id related changes

--- android/incentiviral/src/main/java/com/incentiviral/android/Incentiviral.java
+++ android/incentiviral/src/main/java/com/incentiviral/android/Incentiviral.java
@@ -2,34 +2,63 @@ package com.incentiviral.android;
 
 import android.content.Context;
 
-import java.lang.ref.WeakReference;
-
 /**
  * Created by sohammondal on 20/09/14.
  */
 public class Incentiviral {
     private static Incentiviral sIncentiviral;
-    private static String appId;
+    private static String sAppId;
+    private static Rewards sRewards;
 
-    public static String getAppId() {
-        return appId;
+
+    private static String sUserId;
+
+    private static final String KEY_USER_ID = "user_id";
+
+    public static String getsAppId() {
+        return sAppId;
     }
 
-    private static void setAppId(String appId) {
-        Incentiviral.appId = appId;
+    public static void setUserId(String userId) {
+        Incentiviral.sUserId = sUserId;
     }
 
-    public void setup(final Context context, String appId) {
+    private static void setAppId(String sAppId) {
+        Incentiviral.sAppId = sAppId;
+    }
+
+    public void setup(final Context context, String appId, String userId) {
         setAppId(appId);
+        setUserId(userId);
+        sRewards = fetchRewards(context);
     }
 
+
+    /**
+     * Called by the developer to log an event
+     * @param context
+     * @param eventName the unique event name
+     */
     public void logEvent(final Context context, String eventName) {
 
     }
 
-    public Rewards checkRewards(final Context context) {
+    /**
+     * A method to fetch Rewards from the rewards endpoint
+     * @param context
+     * @return A set of fresh rewards object
+     */
+    public Rewards fetchRewards(final Context context) {
         Rewards rewards = null;
         return rewards;
+    }
+
+    /**
+     * A method to check the status of any reward and then show the reward if needed
+     * @param context
+     */
+    public void checkCurrentRewardsStatus(final Context context) {
+
     }
 
 
---
+++ android/incentiviral/src/main/java/com/incentiviral/android/IncentiviralClient.java
@@ -0,0 +1,8 @@
+package com.incentiviral.android;
+
+/**
+ * Created by sohammondal on 20/09/14.
+ */
+public class IncentiviralClient {
+    private static final String API_URL = "https://api.incentiviral.com";
+}
